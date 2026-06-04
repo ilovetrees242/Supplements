48ba4847562ae1b999b51aacb825e76676a95a38
ilovetrees242 <riyrty594@gmail.com>
2026-06-04 21:09:36 +0530

    Fix base.sh

--- Website/Packages/base.sh
+++ Website/Packages/base.sh
@@ -3,15 +3,6 @@ cat > MOONBUILD << "EOF"
 source "$VEILERDBPATH/sync/(package)/info"
 trap 'echo -e "${BLUE}Interrupted. Goodbye!${NC}"; exit 1' INT
 case $1 in
-    getpkg)
-        if [ ! -f "$MOONPKG.tar.gz" ]; then
-            if [ "$VEILERQUIET" -eq 1 ]; then 
-                wget -q --show-progress ""$SRC"/"$MOONPKG".tar.gz"
-            else 
-                wget -nv --show-progress ""$SRC"/"$MOONPKG".tar.gz"
-            fi
-        fi
-    ;;
     build)
         CONFIGPARAMS=()
         for FLAG in ${USEFLAGS[@]}; do
@@ -21,34 +12,14 @@ case $1 in
                 ;;
             esac
         done
-        if [ -d "$MOONPKG" ]; then rm -rf "$MOONPKG"; fi
-        if [ "$VEILERQUIET" -eq 1 ]; then
-            tar xf "$MOONPKG.tar.gz"
-            pushd "$MOONPKG" &> /dev/null
-                # configure command goes here
-                ./configure --prefix=/usr ${CONFIGPARAMS[@]} > /dev/null
-                # build command goes here
-                make --quiet 
-            popd &> /dev/null
-        else
-            tar xvf "$MOONPKG.tar.gz"
-            pushd "$MOONPKG"
-                # configure command goes here
-                ./configure --prefix=/usr ${CONFIGPARAMS[@]}
-                # build command goes here
-                make
-            popd
-        fi
+        # configure command goes here
+        ./configure --prefix=$VEILERPREFIX ${CONFIGPARAMS[@]}
+        # build command goes here
+        make
     ;;
     install)
-        pushd $MOONPKG &> /dev/null
         # install command goes here
-            if [ $VEILERQUIET -eq 1 ]; then
-                make DESTDIR=$PWD/DEST --quiet install 
-            else
-                make DESTDIR=$PWD/DEST install
-            fi
-        popd &> /dev/null
+        make DESTDIR=$PWD/DEST install
     ;;
 esac
 EOF
